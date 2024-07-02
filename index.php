8a213d2385d34aa5eddbba54caff60e96db78f0e
Alexandr <kotgreen@mail.ru>
2024-07-02 23:57:52 +0300

    add header.php

--- index.php
+++ index.php
@@ -1,18 +1,6 @@
 <?php include('db_connect.php'); ?>
+<?php include('header.php'); ?>
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>task</title>
-    <link rel="stylesheet" href="style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
-
-<body>
-    <h1 id="main_title">My task manager</h1>
 
     <table class="table table-hover" style="width: 900px; margin:auto">
         <thead>
--- new_task.php
+++ new_task.php
@@ -2,8 +2,9 @@
 
 include("db_connect.php");
 
+$date = date("Y-m-d");
 $task = $_POST['add_task'];
-$sql = "INSERT INTO `tasks` (`id`, `created_at`, `task`, `archive`) VALUES (NULL, '2022-05-05', '$task', '0')";
+$sql = "INSERT INTO `tasks` (`id`, `created_at`, `task`, `archive`) VALUES (NULL, '$date', '$task', '0')";
 $pdo->exec($sql);
 
 header('location:index.php');
